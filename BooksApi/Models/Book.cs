d95d5b6db7f77f2bce8b97875af8c6acc753a164
Joshua Partington <joshua.partington@outlook.com>
2022-08-27 11:39:03 +0100

    Add project files.

---
+++ BooksApi/Models/Book.cs
@@ -0,0 +1,18 @@
+﻿namespace BooksApi.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int Price { get; set; }
+
+        public Book(int id, string title, string author, int price)
+        {
+            Id = id;
+            Title = title;
+            Author = author;
+            Price = price;
+        }
+    }
+}
