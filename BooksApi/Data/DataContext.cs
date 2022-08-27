48238ba9808cae272d9422b3ece555dd117887bb
Joshua Partington <joshua.partington@outlook.com>
2022-08-27 14:26:00 +0100

    initial EF setup, added initial migration logic for seeding DB

---
+++ BooksApi/Data/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using BooksApi.Models;
+namespace BooksApi.Data
+{
+    public class DataContext : DbContext
+    {
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        
+        public DbSet<Book> Books { get; set; }
+    }
+}
